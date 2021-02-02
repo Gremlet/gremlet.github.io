30129dd9dfe00199960adcaaeaf23cb667368c87
Gremlet <57172450+Gremlet@users.noreply.github.com>
2021-02-02 19:55:30 +0100

    Add first version

---
+++ precache-manifest.a62ae0c959849d0933ebc38667d588e7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e8aed5e9bf6ccbf2d6",
+    "url": "/css/app.b8dfba84.css"
+  },
+  {
+    "revision": "bb4e2ba34abf767513170f2ffffda4de",
+    "url": "/img/cocktail1.bb4e2ba3.jpg"
+  },
+  {
+    "revision": "e4fd99c6c41f064e0e655432763c2f15",
+    "url": "/img/cocktail2.e4fd99c6.jpg"
+  },
+  {
+    "revision": "22136427f2caab3757e3277fdae520d7",
+    "url": "/img/cocktail3.22136427.jpg"
+  },
+  {
+    "revision": "d7479e37e1f035159c62755b929adbb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91e8aed5e9bf6ccbf2d6",
+    "url": "/js/app.a1367893.js"
+  },
+  {
+    "revision": "8e7a7c1d2707db1ff849",
+    "url": "/js/chunk-vendors.352c255c.js"
+  },
+  {
+    "revision": "ac4a0913bf5163bfe148ad9efda381d1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
